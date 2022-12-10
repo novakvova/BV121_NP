bd26e094a99ff5bc350cebadaa56641e6215c74d
novakvova <novakvova@gmail>
2022-12-10 19:35:56 +0200

    sss

--- ChatServerConsole/Program.cs
+++ ChatServerConsole/Program.cs
@@ -56,8 +56,8 @@ namespace ChatServerConsole
                 byte[] buffer = new byte[4096]; //щоб якщо буде велике повідомлення, то має влізти
                 int byte_count = stream.Read(buffer); //прочиталі дані від клієнта
                 if (byte_count == 0) break; //якщо клієнта пислав пустоту, то ми зним прощаємося
-                broadcast(buffer); //розіслали повідомлення усім клєінта хто є в чаті.
                 string data = Encoding.UTF8.GetString(buffer); //отримали текстове повідомлення від клієнта
+                broadcast(data); //розіслали повідомлення усім клєінта хто є в чаті.
                 Console.WriteLine(data); //показуємо повідомення, що прислав клієнт
             }
             lock(_lock) { list_clients.Remove(id); }
@@ -65,11 +65,13 @@ namespace ChatServerConsole
             client.Close();
         }
         //Відправляємо повідомлення усім клієнтам, які є в чаті - метод broadcast
-        public static void broadcast(byte[] buffer)
+        public static void broadcast(string data)
         {
-            //byte[] buffer = Encoding.UTF8.GetBytes(data);
+            byte[] buffer = Encoding.UTF8.GetBytes(data);
+            
             lock(_lock) //Щоб повідомлення при розсилці не перемішувалися
             {
+                Console.WriteLine("Send data client: {0}", data);
                 foreach(TcpClient c in list_clients.Values) //перебираємо усі клієнтів
                 {
                     NetworkStream stream = c.GetStream(); //отримуємо потік окремого клієнта
