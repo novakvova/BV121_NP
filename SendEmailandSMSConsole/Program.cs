e8528a84301355681c23eb31c85f608c4ca0b61d
novakvova <novakvova@gmail>
2022-12-13 19:18:17 +0200

    Send sms is good

--- SendEmailandSMSConsole/Program.cs
+++ SendEmailandSMSConsole/Program.cs
@@ -9,13 +9,17 @@ namespace SendEmailandSMSConsole
         {
             Console.InputEncoding = Encoding.UTF8;
             Console.OutputEncoding = Encoding.UTF8;
-            Console.WriteLine("Відправка повідомлення novakvova@gmail.com!");
-            SmtpEmailService emailService = new SmtpEmailService();
-            Message message = new Message();
-            message.Subject = "Закупляємо альтернативні джерела енергії";
-            message.Body = "Беріть гернератори, інвертери або свічки і усе буде круто";
-            message.To = "novakvova@gmail.com";
-            emailService.Send(message);
+            //Console.WriteLine("Відправка повідомлення novakvova@gmail.com!");
+            //SmtpEmailService emailService = new SmtpEmailService();
+            //Message message = new Message();
+            //message.Subject = "Закупляємо альтернативні джерела енергії";
+            //message.Body = "Беріть гернератори, інвертери або свічки і усе буде круто";
+            //message.To = "novakvova@gmail.com";
+            //emailService.Send(message);
+
+            //SMSService sms = new SMSService();
+            //sms.Send("38097174909890", "Доброго вечора! Виздоровлюйте швидше. Нехай буде мирне небо. :)");
+
         }
     }
 }
