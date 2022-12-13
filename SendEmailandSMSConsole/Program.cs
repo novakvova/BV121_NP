55a12523a229ec7e6d2ec7b42a8cfbc6d8b8dffc
novakvova <novakvova@gmail>
2022-12-13 18:51:58 +0200

    Send email Message ukr.net server is good

---
+++ SendEmailandSMSConsole/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Text;
+
+namespace SendEmailandSMSConsole
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.InputEncoding = Encoding.UTF8;
+            Console.OutputEncoding = Encoding.UTF8;
+            Console.WriteLine("Відправка повідомлення novakvova@gmail.com!");
+            SmtpEmailService emailService = new SmtpEmailService();
+            Message message = new Message();
+            message.Subject = "Закупляємо альтернативні джерела енергії";
+            message.Body = "Беріть гернератори, інвертери або свічки і усе буде круто";
+            message.To = "novakvova@gmail.com";
+            emailService.Send(message);
+        }
+    }
+}
